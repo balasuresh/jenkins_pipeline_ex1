6a0cb803ff50d82c02ea9fa17cf28c4306e1c671
balasuresh <korumilli.balasuresh@gmail.com>
2019-08-06 19:44:45 +0530

    comitted

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,16 @@
 package com.mycompany.app;
 
 /**
- * Hello world!
+ * Hello world Test Enveronment!
  */
 public class App
 {
-
     private final String message = "Hello World!";
-
     public App() {}
-
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
-
     private final String getMessage() {
         return message;
     }
-
 }
